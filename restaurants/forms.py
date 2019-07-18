50eb23dcc304fb754fd6de5842300f27cb879753
Ahmad Helal <you@example.com>
2019-07-18 13:02:11 +0300

    C

--- restaurants/forms.py
+++ restaurants/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Restaurant
+from django.contrib.auth.models import User
 
 class RestaurantForm(forms.ModelForm):
     class Meta:
@@ -10,3 +11,19 @@ class RestaurantForm(forms.ModelForm):
         	'opening_time': forms.TimeInput(attrs={'type':'time'}),
         	'closing_time': forms.TimeInput(attrs={'type':'time'}),
         }
+
+
+class SignUpForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name', 'email' ,'password']
+
+        widgets={
+        'password': forms.PasswordInput(),
+        }
+
+
+class Signin(forms.Form):
+	user_name= forms.CharField(required=True, max_length= 150)
+	password=forms.CharField(required=True, widget=forms.PasswordInput())
+
--- restaurants/views.py
+++ restaurants/views.py
@@ -1,18 +1,48 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth import login, logout,  authenticate
 from .models import Restaurant
-from .forms import RestaurantForm
+from .forms import RestaurantForm, SignUpForm, Signin
 
 def signup(request):
-    
-    return render(request, 'signup.html', context)
+    form=SignUpForm()
+    if request.method=='POST':
+        form=SignUpForm(request.POST)
+        if form.is_valid:
+            user= form.save(commit=False)
+            user.set_password(user.password)
+            user.save()
+            login (request,user)
+            return redirect("restaurant-list")
+    context= {
+    "form": form,
+    }
 
+    return render(request, 'signup.html', context)
+    message=""
 def signin(request):
+    form= Signin()
+    if request.method=='POST':
+        form=Signin (request.POST)
+        if form.is_valid():
+            user_name= form.cleaned_data['user_name']
+            password=form.cleaned_data['password']
+            authentication= authenticate(username=user_name, password=password)
+            if authentication is not None:
+                login(request, authentication)
+                return redirect('restaurant-list')
+            else:
+                message= "Re-enter password"
+    context={
+    "form":form,
+    "m": message,
+    }
+
     
-    return 
+    return render(request, 'signin.html', context)
 
 def signout(request):
-    
-    return 
+    logout (request)
+    return redirect(request, 'signin.html', context)
 
 def restaurant_list(request):
     context = {
